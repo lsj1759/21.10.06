7259de97637333927ee88b7a7b7a2e99aa60562d
lsj1759 <lsj1759@naver.com>
2021-10-06 17:22:13 +0900

    string 추가

--- 21.10.06/source.cpp
+++ 21.10.06/source.cpp
@@ -1,64 +1,136 @@
 #include <iostream>
-#include "Engine.h"
-#include "Actor.h"
-#include <vector> //STL
+#include "engine.h"
+#include "actor.h"
+#include <vector> //stl
+#include <list>
+#include <algorithm>
+#include <string>
+
+
+
 
 using namespace std;
 
+int main()
+{
+	string HelloWorld = "Hello World";
+
+	for (auto S : HelloWorld)
+	{
+		cout << static_cast<char>(toupper(S));
+	}
+
+	return 0;
+}
+
+
+
+
+int main()
+{
+	list<int> intliststore;
+	for (int i = 1; i <= 10; ++i)
+	{
+		intliststore.push_back(i);
+	}
+	
+	for (auto v : intliststore) //range for
+	{
+		cout << v << endl;
+	}
+	
+	
+	for (auto iter = intliststore.begin(); iter != intliststore.end(); ++iter)
+	/*for (list<int>::iterator iter = intliststore.begin(); iter != intliststore.end(); ++iter)*/
+	{
+		cout << *iter << endl;
+	}
+
+	
+	
+	
+	vector<int> intstore;
+
+	for (int i = 1; i <= 10; ++i)
+	{
+		intstore.push_back(i);
+	}
+
+	intstore.erase(intstore.begin() + 3);
+	intstore.insert(intstore.begin() + 2, 11);
+	
+	for (size_t i = 0; i < intstore.size(); ++i)
+	{
+		cout << intstore[i] << endl;
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
 //template function
-template<typename T, typename T1, typename T2>
-T2 Add(T a, T1 b)
+template<typename t, typename t1, typename t2>
+t2 add(t a, t1 b)
 {
 	return a + b;
 }
 
 //template class
-template<typename T>
-class NumerStore
+template<typename t>
+class numerstore
 {
 public:
-	T Store[100];
+	t store[100];
 };
 
 //function overloding
-int Add(int A, int B)
+int add(int a, int b)
 {
-	return A + B;
+	return a + b;
 }
 
-float Add(float A, float B)
+float add(float a, float b)
 {
-	return A + B;
+	return a + b;
 }
 
 
 
 int main()
 {
-	NumerStore<int> IntStore;
-	NumerStore<float> FloatStore;
+	numerstore<int> intstore;
+	numerstore<float> floatstore;
 
-	FloatStore.Store[0] = 10.0f;
-	IntStore.Store[0] = 10;
+	floatstore.store[0] = 10.0f;
+	intstore.store[0] = 10;
 
 	int a = 2;
 	int b = 3;
 
-	cout << Add<int, int, int>(a, b) << endl;
-	cout << Add<int, float, float>(2, 3.0f) << endl;
+	cout << add<int, int, int>(a, b) << endl;
+	cout << add<int, float, float>(2, 3.0f) << endl;
 
+	return 0;
+}
 
-	//	Engine* engine = new Engine();
-	//
-	//	(*engine).SpawnActor();
-	//	 //Wall, Player, Goal(Object)
-	//	engine->SpawnActor();
-	//
-	////	engine->DestroyActor(0);
-	//
-	//	engine->Run();
-	//
-	//	delete engine;
-	//
+int main()
+{
+	Engine* engine = new Engine();
+
+	(*engine).SpawnActor();
+	//wall, player, goal(object)
+	engine->SpawnActor();
+
+	//	engine->destroyactor(0);
+
+	engine->Run();
+
+	delete engine;
+
 	return 0;
 }
